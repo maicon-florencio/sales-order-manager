a5b70119fa6747250b64769411b53f26af2d2589
Maicon Silva Florencio <msflorencio@stefanini.com>
2022-11-28 21:21:33 -0300

    remocao de imports nao usados

--- produto/src/main/java/com/microsservice/learning/produto/service/mapper/ProdutoMapper.java
+++ produto/src/main/java/com/microsservice/learning/produto/service/mapper/ProdutoMapper.java
@@ -6,7 +6,6 @@ import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 
